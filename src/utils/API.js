8936c6ca3517bf1a1c41c4c054832d349a796a23
beng888 <lawrenceardosa@gmail.com>
2021-01-22 22:53:47 +0800

    commit

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  // baseURL: process.env.REACT_APP_URL,
+  baseURL: process.env.REACT_APP_URL,
   withCredentials: true,
   credentials: "include",
 });
